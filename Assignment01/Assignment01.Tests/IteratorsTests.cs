4b04edd02d2d3f679a42805ac3f1ad711bd120d4
Maxime Birkkjær Havez <maxih@hotmail.dk>
2022-09-14 15:13:52 +0200

    Added test for filter method

--- Assignment01/Assignment01.Tests/IteratorsTests.cs
+++ Assignment01/Assignment01.Tests/IteratorsTests.cs
@@ -11,9 +11,20 @@ public class IteratorsTests
         numbers.Add(new List<int>{4,5,6});
         numbers.Add(new List<int>{7,8,9});
         // When
-        var result = Iterators.Flatten<int>(numbers);
+        var result = Iterators.Flatten(numbers);
         // Then
         Assert.Equal(new[]{1,2,3,4,5,6,7,8,9},result);
+    }
+    [Fact]
+    public void Return_1_3_5_Given_1_2_3_4_5()
+    {
+        // Given
+        var numbers = new[]{1,2,3,4,5};
+        bool Odd(int num) => num%2==1;
+        // When
+        var result = Iterators.Filter(numbers,Odd);
+        // Then
+        result.Should().BeEquivalentTo(new[]{1,3,5});
     }
 
 }
