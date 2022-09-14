a3a24883dcbc6e557109a51519cf139a587f14d9
katrinesando <kmsa@itu.dk>
2022-09-14 16:07:29 +0200

    test to Resolution

--- Assignment01/Assignment01.Tests/RegExprTests.cs
+++ Assignment01/Assignment01.Tests/RegExprTests.cs
@@ -14,4 +14,38 @@ public class RegExprTests
         //Then
         result.Should().BeEquivalentTo(new[]{"This","is","a", "String", "of","4", "words","yay"});
     }
+
+    [Fact]
+    public void Resolution_width_height_single_input()
+    {
+        //Given
+        var screenRes = "1920x1080";
+        //When
+        var result = RegExpr.Resolution(screenRes);
+        
+        //then
+        result.Should().BeEquivalentTo(1920,1080);
+    }
+    [Fact]
+    public void Resolution_width_height_multiple_input()
+    {
+        //Given
+        var screenRes = new[]{"1024x768", "800x600", "640x480"}
+        //When
+        var result = RegExpr.Resolution(screenRes);
+        
+        //then
+        result.Should().BeEquivalentTo((1024,768),(800,600),(640,480));
+    } 
+    [Fact]
+    public void Resolution_width_height_multiple_input_single_line()
+    {
+        //Given
+        var screenRes = new[]{"1024x768,800x600,640x480"}
+        //When
+        var result = RegExpr.Resolution(screenRes);
+        
+        //then
+        result.Should().BeEquivalentTo((1024,768),(800,600),(640,480));
+    }
 }
