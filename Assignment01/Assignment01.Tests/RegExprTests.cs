5bf756772e8f53612e6bba585963c3d4cfbc5dba
katrinesando <kmsa@itu.dk>
2022-09-14 15:31:31 +0200

    test added to splitline function

--- Assignment01/Assignment01.Tests/RegExprTests.cs
+++ Assignment01/Assignment01.Tests/RegExprTests.cs
@@ -2,4 +2,16 @@ namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+    [Fact]
+    public void Regex_should_return_singular_words()
+    {
+        //Given
+        var test = new[]{"This is a String of 4 words","yay"};
+
+        //When
+        var result = RegExpr.Splitline(test);
+
+        //Then
+        result.Should().BeEquivalentTo(new[]{"This","is","a", "String", "of","4", "words","yay"});
+    }
 }
